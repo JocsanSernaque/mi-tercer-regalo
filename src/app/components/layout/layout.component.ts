3ef4e72cec70edc011b3de1713833bacf372fbd4
Jocsan <jocsansernaque@gmail.com>
2025-08-05 18:10:50 -0500

    create section home, products an about

---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
+import { AboutComponent } from "../about/about.component";
+import { ProductsComponent } from "../products/products.component";
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [NavbarComponent, RouterOutlet, FooterComponent, AboutComponent, ProductsComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
